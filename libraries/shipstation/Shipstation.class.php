aa52984452e2d11bf28fd81ae36ab3292e7529bc
grafikchaos <josh@grafikchaos.com>
2016-06-29 14:50:32 -0500

    Refactor all private variables/methods to protected so the class can be extended

--- libraries/shipstation/Shipstation.class.php
+++ libraries/shipstation/Shipstation.class.php
@@ -18,26 +18,26 @@ class ShipStation
 {
 
 	// ShipStation credentials //
-	private $ssApiKey;
-	private $ssApiSecret;
-	private $authorization;
-    private $defaultAuthorization;
+	protected $ssApiKey;
+	protected $ssApiSecret;
+	protected $authorization;
+    protected $defaultAuthorization;
 
 
 	// Shipstation endpoint & methods //
-	private $endpoint;
-	private $methodsPaths;
+	protected $endpoint;
+	protected $methodsPaths;
 
 
     // Error Handling //
-    private $lastError;
+    protected $lastError;
 
 
     // Requests cap handling //
 
-    private $remainingRequests;
-    private $resetTime;
-    private $lastRequestTime;
+    protected $remainingRequests;
+    protected $resetTime;
+    protected $lastRequestTime;
 
 
 
@@ -45,23 +45,23 @@ class ShipStation
     * ----------------------------------------------------
     *   _construct()
     * ----------------------------------------------------
-    * 
+    *
     * Instantiate ShipStation Class.
-    * 
-    * @return  Object ShipStation   
+    *
+    * @return  Object ShipStation
     */
 
     public function __construct()
     {
 
     	// Define Endpoint //
-    	
+
     	$this->endpoint 		= 'https://ssapi.shipstation.com/';
 
 
     	// Define Default Credentials //
     	// Authorization Token = The String: "{SS API Key}:{SS API Secret}" encoded with base64 //
-    	// 
+    	//
 		$this->ssApiKey			= null;
 		$this->ssApiSecret		= null;
 		$this->authorization 	= 'Basic {Your Authorization Token Here}';
@@ -78,7 +78,7 @@ class ShipStation
 
 		$this->methodsPaths 	= array
 		(
-            
+
             // Order related methods //
 
             'getOrders'         => 'Orders',
@@ -99,12 +99,12 @@ class ShipStation
 
 			'getWarehouses' 	=> 'warehouses',
 
-            
+
 			// Stores related methods //
 
             'getStores'         => 'Stores',
-            
-            
+
+
             // Carriers related methods //
 
             'getCarriers'         => 'carriers',
@@ -126,9 +126,9 @@ class ShipStation
     * ----------------------------------------------------
     *  getOrders($filters)
     * ----------------------------------------------------
-    * 
+    *
     * Get a list of all orders on ShipStation matching the filter.
-    * 
+    *
     * @param    Array $filters
     *
     * @return   Array $orders
@@ -164,7 +164,7 @@ class ShipStation
                 "Authorization" => $this->authorization
             )
         );
-        
+
         return $this->processReply($response);
 
     }
@@ -214,9 +214,9 @@ class ShipStation
     * ----------------------------------------------------
     *  getOrder($orderId)
     * ----------------------------------------------------
-    * 
+    *
     * Get an specific order on ShipStation by its ID.
-    * 
+    *
     * @param    int         $orderId
     *
     * @return   stdClass    $order
@@ -238,7 +238,7 @@ class ShipStation
                 "Authorization" => $this->authorization
             )
         );
-        
+
         return $this->processReply($response);
 
     }
@@ -247,22 +247,22 @@ class ShipStation
      * --------------------------------------------------
      * addTagToOrder($orderId, $tagId)
      * --------------------------------------------------
-     * 
+     *
      * Add a Tag to a Shipstation Order.
-     * 
+     *
      * @param    int         $orderId
      * @param    int         $tagId
-     * 
+     *
      * @return   stdClass    $order
      */
-     
+
      public function addTagToOrder($orderId, $tagId)
      {
-     	
+
      	// Enforce API requests cap //
 
         $this->enforceApiRateLimit();
-        
+
         $response = Unirest::post
         (
             $this->endpoint.$this->methodsPaths['addTagToOrder'],
@@ -279,29 +279,29 @@ class ShipStation
         );
 
         return $this->processReply($response);
-     	
+
      }
 
     /**
     * ----------------------------------------------------
     *  addOrder($order)
     * ----------------------------------------------------
-    * 
+    *
     * Add a new order to ShipStation.
-    * 
+    *
     * @return stdClass $order
     */
 
     public function addOrder($order)
     {
-        
+
         // Enforce API requests cap //
 
         $this->enforceApiRateLimit();
 
         // The API can't handle empty or null values on filters... (¬¬)
         // Validation of types would be useful.
-        
+
         foreach($order as $property => $value)
         {
             if(empty($value))
@@ -331,9 +331,9 @@ class ShipStation
     * ----------------------------------------------------
     *  deleteOrder($orderId)
     * ----------------------------------------------------
-    * 
+    *
     * Delete an order on ShipStation by its ID.
-    * 
+    *
     * @param    int         $orderId
     *
     * @return   void
@@ -355,7 +355,7 @@ class ShipStation
                 "Authorization" => $this->authorization
             )
         );
-        
+
         return $this->processReply($response);
 
     }
@@ -364,7 +364,7 @@ class ShipStation
     // Orders Related Methods [END] ============================== //
     // =========================================================== //
 
-    
+
 
     // Shipment Related Methods [START] ========================== //
     // =========================================================== //
@@ -374,9 +374,9 @@ class ShipStation
     * ----------------------------------------------------
     *  getShipments($filters)
     * ----------------------------------------------------
-    * 
+    *
     * Get a list of all shipments on ShipStation matching the filter.
-    * 
+    *
     * @param    Array $filters
     *
     * @return   Array $shipments
@@ -412,7 +412,7 @@ class ShipStation
                 "Authorization" => $this->authorization
             )
         );
-        
+
         return $this->processReply($response);
 
     }
@@ -468,9 +468,9 @@ class ShipStation
     * ----------------------------------------------------
     *  getRates($filters)
     * ----------------------------------------------------
-    * 
-    * Retrieves shipping rates for the specified shipping details. 
-    * 
+    *
+    * Retrieves shipping rates for the specified shipping details.
+    *
     * @param    Array $filters
     *
     * @return   Array $rates
@@ -505,21 +505,21 @@ class ShipStation
                 "Authorization" => $this->authorization,
                 "Content-type" => "application/json"
             ),
-            json_encode( $filters )      
+            json_encode( $filters )
         );
-        
+
         return $this->processReply($response);
 
     }
-    
+
     /**
     * ----------------------------------------------------
     *  createLabel($filters)
     * ----------------------------------------------------
-    * 
+    *
     * Creates a shipping label. The labelData field returned in the response is a base64 encoded PDF value.
     * Simply decode and save the output as a PDF file to retrieve a printable label.
-    * 
+    *
     * @param    Array $filters
     *
     * @return   Array $label
@@ -554,12 +554,12 @@ class ShipStation
                 "Authorization" => $this->authorization,
                 "Content-type" => "application/json"
             ),
-            json_encode( $filters )      
+            json_encode( $filters )
         );
         return $this->processReply($response);
 
     }
-    
+
     // Shipment Related Methods [END] ============================ //
     // =========================================================== //
 
@@ -572,9 +572,9 @@ class ShipStation
     * ----------------------------------------------------
     *  getWarehouses()
     * ----------------------------------------------------
-    * 
+    *
     * Get list of warehouses availables.
-    * 
+    *
     * @return Array $warehouses
     */
 
@@ -610,9 +610,9 @@ class ShipStation
     * ----------------------------------------------------
     *  getStores()
     * ----------------------------------------------------
-    * 
+    *
     * Get list of stores available.
-    * 
+    *
     * @return Array $stores
     */
 
@@ -640,7 +640,7 @@ class ShipStation
     // Stores Related Methods [END] ============================== //
     // =========================================================== //
 
-    
+
     // Carriers Related Methods [START] ============================ //
     // =========================================================== //
 
@@ -648,9 +648,9 @@ class ShipStation
     * ----------------------------------------------------
     *  getCarriers()
     * ----------------------------------------------------
-    * 
+    *
     * Get list of carriers available.
-    * 
+    *
     * @return Array $carriers
     */
 
@@ -673,14 +673,14 @@ class ShipStation
         return $this->processReply($response);
 
     }
-    
+
      /**
     * ----------------------------------------------------
     *  getCarrier($carrierCode)
     * ----------------------------------------------------
-    * 
+    *
     * Get attributes of Carrier matching provided carrierCode
-    * 
+    *
     * @param    String $carrierCode
     *
     * @return   Object $carrier
@@ -702,18 +702,18 @@ class ShipStation
                 "Authorization" => $this->authorization
             )
         );
-        
+
         return $this->processReply($response);
 
     }
-    
+
     /**
     * ----------------------------------------------------
     *  getPackages($carrierCode)
     * ----------------------------------------------------
-    * 
+    *
     * Get a list of all Packages offered by the supplied carrierCode
-    * 
+    *
     * @param    String $carrierCode
     *
     * @return   Array $packages
@@ -735,18 +735,18 @@ class ShipStation
                 "Authorization" => $this->authorization
             )
         );
-        
+
         return $this->processReply($response);
 
     }
-    
+
     /**
     * ----------------------------------------------------
     *  getServices($carrierCode)
     * ----------------------------------------------------
-    * 
+    *
     * Get a list of all Services offered by the supplied carrierCode
-    * 
+    *
     * @param    String $carrierCode
     *
     * @return   Array $services
@@ -769,7 +769,7 @@ class ShipStation
                 "Authorization" => $this->authorization
             )
         );
-        
+
         return $this->processReply($response);
 
     }
@@ -785,9 +785,9 @@ class ShipStation
     * ----------------------------------------------------
     *  setLastError()
     * ----------------------------------------------------
-    * 
+    *
     * Sets the error object for last failed request.
-    * 
+    *
     * @return void
     */
 
@@ -810,15 +810,15 @@ class ShipStation
     * ----------------------------------------------------
     *  getLastError()
     * ----------------------------------------------------
-    * 
+    *
     * Returns the last response from server, reported as error.
-    * 
+    *
     * @return stdClass $error
     */
 
     public function getLastError()
     {
-        
+
         return $this->lastError;
 
     }
@@ -836,15 +836,15 @@ class ShipStation
     * ----------------------------------------------------
     *  processReply($response)
     * ----------------------------------------------------
-    * 
+    *
     * Process reply from server, intended to add further validation/handling.
-    * 
+    *
     * @return stdClass $object
     */
 
-    private function processReply($response)
+    protected function processReply($response)
     {
-     
+
 
         // API cap handling + error handling //
         if(is_object($response))
@@ -889,15 +889,15 @@ class ShipStation
     * ----------------------------------------------------
     *  setSsApiKey($ssApiKey)
     * ----------------------------------------------------
-    * 
+    *
     * Sets ShipStation Api Key.
-    * 
+    *
     * @return void
     */
 
     public function setSsApiKey($ssApiKey)
     {
-        
+
         $this->ssApiKey = $ssApiKey;
 
         if(!empty($this->ssApiSecret))
@@ -913,15 +913,15 @@ class ShipStation
     * ----------------------------------------------------
     *  setSsApiSecret($ssApiSecret)
     * ----------------------------------------------------
-    * 
+    *
     * Sets ShipStation Api Secret.
-    * 
+    *
     * @return void
     */
 
     public function setSsApiSecret($ssApiSecret)
     {
-        
+
         $this->ssApiSecret = $ssApiSecret;
 
         if(!empty($this->ssApiKey))
@@ -937,16 +937,16 @@ class ShipStation
     * ----------------------------------------------------
     *  setAuthorization($authorization)
     * ----------------------------------------------------
-    * 
+    *
     * Sets the authorization token to use directly
     * allowing to switch between multiple ShipStation Accounts faster.
-    * 
+    *
     * @return void
     */
 
     public function setAuthorization($authorization)
     {
-        
+
         $this->authorization    = $authorization;
 
     }
@@ -956,15 +956,15 @@ class ShipStation
     * ----------------------------------------------------
     *  resetAuthorization()
     * ----------------------------------------------------
-    * 
+    *
     * Resets the authorization token to default.
-    * 
+    *
     * @return void
     */
 
     public function resetAuthorization()
     {
-        
+
         $this->authorization    =  $this->defaultAuthorization;
 
     }
@@ -975,9 +975,9 @@ class ShipStation
     * ----------------------------------------------------
     *  enforceApiRateLimit()
     * ----------------------------------------------------
-    * 
+    *
     * Enforces ShipStation API
-    * 
+    *
     * @return stdClass $object
     */
 
@@ -987,19 +987,19 @@ class ShipStation
     /* but this code should handle it if the cap is ever to be honored...
 	*/
 
-    private function enforceApiRateLimit()
+    protected function enforceApiRateLimit()
     {
-        
+
         if($this->remainingRequests>0)
         {
             return;
         }
         else
         {
-            
+
             if( !empty($this->lastRequestTime) )
             {
-                
+
                 $elapsedTime        = ( time() - $this->lastRequestTime );
 
                 if( $elapsedTime > $this->resetTime )
@@ -1009,7 +1009,7 @@ class ShipStation
                 else
                 {
                     $waitingTime    = ( $this->resetTime - $elapsedTime );
-                    
+
                     sleep($waitingTime);
                 }
 
@@ -1019,7 +1019,7 @@ class ShipStation
                 return; // We never should get here...
             }
 
-            
+
         }
 
     }
